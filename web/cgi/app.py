6beebc083b6da5ab179d73396c6c1b38cd3d2c2c
sdjl <sdjllyh@gmail.com>
2011-10-10 15:12:36 +0800

    firstcommit

---
+++ web/cgi/app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import web, os , nginx_uwsgi ,  GetColorCode
+web.config.debug = False
+
+app = web.application(('/cgi/getcc','GetColorCode',), {'GetColorCode':GetColorCode.GetColorCode})
+
+app.notfound = lambda:web.seeother('/')
+
+if __name__ == "__main__":
+    app.run()
