e7764f54a7b3bb48fbd3e7c8fee0f16ed1d6dfcd
sdjl <sdjllyh@gmail.com>
2011-10-10 18:53:02 +0800

    添加复制功能

--- web/cgi/GetColorCode.py
+++ web/cgi/GetColorCode.py
@@ -1,12 +1,13 @@
 #coding=utf8
-import web, site_helper, os
+import web, site_helper, os, uuid
 
 class GetColorCode:
     def POST(self):
         i = web.input()
-        file_name = 'temp.py'
+        assert(i.has_key('type') and i.type.isalnum())
+        file_name = str(uuid.uuid4())+'.'+i.type
         f = open(site_helper.config.APP_ROOT_PATH+'web/codes/'+file_name,'w')
-        f.write(i.code)
+        f.write(i.code.encode('utf8'))
         f.close()
-        os.system('cd %s\n vim -f +"set nonumber" +"let g:html_use_css = 0" +"syn on" +"run! syntax/2html.vim" +"wq" +"q" %s' % (site_helper.config.APP_ROOT_PATH+'web/codes/',file_name))
+        os.system('cd %s\n vim -f +"let g:html_use_css = 0" +"syn on" +"run! syntax/2html.vim" +"wq" +"q" %s' % (site_helper.config.APP_ROOT_PATH+'web/codes/',file_name))
         return open(site_helper.config.APP_ROOT_PATH+'web/codes/'+file_name+'.html').read().partition('<body')[2].partition('>')[2].rpartition('</body>')[0]
--- web/js/colorcode.js
+++ web/js/colorcode.js
@@ -1,8 +1,17 @@
 $(function(){
+
     test = function(){
-        data = {code:$('#blackcode').val()};
+        data = {code:$('#blackcode').val(), type:'py'};
         $.post('/zarkapi/getcolorcode',data,function(cc){
             $('#showcolorcode').html(cc);
         },'text');
-    }
+    };
+
+    $.clipboardReady(function(){
+        $("#copy").click(function(){
+            $.clipboard( "You clicked on a link and copied this text!" );
+            return false;
+        });
+    }, { swfpath: "/swf/jquery.clipboard.swf"} );
+
 });
