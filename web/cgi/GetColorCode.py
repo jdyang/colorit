3fbe52900a5e7131e4d2c1ca25e58920d36f675b
sdjl <sdjllyh@gmail.com>
2011-10-11 14:30:01 +0800

    初步实现colorit的交互

--- web/cgi/GetColorCode.py
+++ web/cgi/GetColorCode.py
@@ -4,8 +4,8 @@ import web, site_helper, os, uuid
 class GetColorCode:
     def POST(self):
         i = web.input()
-        assert(i.has_key('type') and i.type.isalnum())
-        file_name = str(uuid.uuid4())+'.'+i.type
+        assert(i.has_key('type') and i.type.strip().isalnum())
+        file_name = str(uuid.uuid4())+'.'+i.type.strip()
         f = open(site_helper.config.APP_ROOT_PATH+'web/codes/'+file_name,'w')
         f.write(i.code.encode('utf8'))
         f.close()
