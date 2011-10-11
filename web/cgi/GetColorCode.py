6dd86e7c58ad5581a2fdeb2aecdb45a78d51d360
sdjl <sdjllyh@gmail.com>
2011-10-11 19:11:23 +0800

    bugs

--- web/cgi/GetColorCode.py
+++ web/cgi/GetColorCode.py
@@ -5,6 +5,7 @@ class GetColorCode:
     def POST(self):
         i = web.input()
         assert(i.has_key('type') and i.type.strip().isalnum())
+        assert(i.has_key('options'))
         file_name = str(uuid.uuid4())+'.'+i.type.strip()
         f = open(site_helper.config.APP_ROOT_PATH+'web/codes/'+file_name,'w')
         f.write(i.code.encode('utf8'))
@@ -20,11 +21,11 @@ class GetColorCode:
         return code_string.partition('<%s' % tag_name)[2].partition('>')[2].rpartition('</%s>' % tag_name)[0]
 
     def _getFormatFile(self, i):
-        if ('format' and 'number') in i.keys():
+        if ('format' and 'number') in i.options:
             return 'number_format'
-        elif 'format' in i.keys():
+        elif 'format' in i.options:
             return 'format'
-        elif 'number' in i.keys():
+        elif 'number' in i.options:
             return 'number'
         else:
             return 'simple'
