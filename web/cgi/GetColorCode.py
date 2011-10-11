2a9645781ac6a8fa31e549b6f3f4d4f83f28e8b4
sdjl <sdjllyh@gmail.com>
2011-10-11 16:31:37 +0800

    添加了format功能

--- web/cgi/GetColorCode.py
+++ web/cgi/GetColorCode.py
@@ -9,11 +9,10 @@ class GetColorCode:
         f = open(site_helper.config.APP_ROOT_PATH+'web/codes/'+file_name,'w')
         f.write(i.code.encode('utf8'))
         f.close()
-        os.system('cd %s\n vim -f +"let g:html_use_css = 0" +"syn on" +"run! syntax/2html.vim" +"wq" +"q" %s' % (site_helper.config.APP_ROOT_PATH+'web/codes/',file_name))
+        assert(os.path.exists(site_helper.config.APP_ROOT_PATH+'web/vimscriptin/format'))
+        os.system('cd %s\n vim  -f -s "%s" %s' % ( site_helper.config.APP_ROOT_PATH+'web/codes/', site_helper.config.APP_ROOT_PATH+'web/vimscriptin/format', file_name ))
         code_string = open(site_helper.config.APP_ROOT_PATH+'web/codes/'+file_name+'.html').read()
         code_string = self._stripTag(code_string, 'body')
-        #code_string = self._stripTag(code_string, 'font')
-        #code_string = code_string.replace('font', 'span')
         return code_string
 
     def _stripTag(self, code_string, tag_name):
